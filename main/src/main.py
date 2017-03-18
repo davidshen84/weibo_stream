7189133fdbdf142f4b857830fb3900aad8146e7c
Xi Shen <davidshen84@gmail.com>
2017-03-18 14:03:30 +0800

    Implement `get_mongo_client' and refactor.

--- main/src/main.py
+++ main/src/main.py
@@ -11,17 +11,17 @@ Wraps `weibo`_ public statues API into a stream like API.
 .. _API documents: http://open.weibo.com/wiki/%E5%BE%AE%E5%8D%9AAPI
 .. _chunked: https://en.wikipedia.org/wiki/Chunked_transfer_encoding
 """
-from pymongo import MongoClient
 from tornado import web
 from tornado.ioloop import IOLoop
 from tornado.options import define, options
 
 from controllers import app_log, DefaultHandler, PublicTimelineHandler, WeiboStatusCrawlerHandler
+from modules import get_mongo_client
 from modules.util import CircularList
 from modules.weibo_client import WeiboClient
 
 define('weibo_access_tokens', multiple=True)
-define('mongodb_uri', default='mongodb://mongodb:27017/')
+define('mongo_client_factory', default='local')
 define('debug', default=False)
 
 if __name__ == '__main__':
@@ -33,11 +33,11 @@ if __name__ == '__main__':
         (r'/v2/job/(?P<action>\w*)', WeiboStatusCrawlerHandler,
          {'weibo_access_tokens': CircularList(options.weibo_access_tokens),
           'weibo_client_factory': lambda token: WeiboClient(token),
-          'mongo_client': MongoClient(options.mongodb_uri)}, 'job'),
+          'mongo_client': get_mongo_client(options.mongo_client)}, 'job'),
         (r'/.*', DefaultHandler)],
         debug=options.debug)
     # listen to default HTTP port
-    app.listen(8080)
+    app.listen(8088)
     try:
         IOLoop.current().start()
     except KeyboardInterrupt:
--- main/src/modules/__init__.py
+++ main/src/modules/__init__.py
@@ -2,4 +2,22 @@
 Modules
 =======
 
+Module as in MVC.
 """
+
+from pymongo import MongoClient
+
+
+def get_mongo_client(factory: str) -> MongoClient:
+    """
+    Create a MongoClient instance based on the given `factory` name.
+
+    :param factory: name of the factory
+    :return: MongoClient instance
+    """
+    if factory == 'local':
+        return MongoClient('localhost')
+    elif factory == 'docker':
+        return MongoClient('mongo')
+    else:
+        raise ValueError('unknown factory')
